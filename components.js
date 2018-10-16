718f09690405682a476455460d6052aaaadba85d
Jim Doyle <indellibleink@gmail.com>
2018-10-16 13:31:28 -0400

    Started Code School React

---
+++ components.js
@@ -0,0 +1,8 @@
+class StoryBox extends React.Component {
+    render() {
+        return( <div>Story Box</div>);
+    }
+}
+ReactDON.render(
+    <StoryBox />,document.getElementById('story-app')
+);
