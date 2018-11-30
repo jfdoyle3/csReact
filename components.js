f9c28bc2f03d333e4f5b1743e4afb42fa3ac54d6
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-11-30 08:07:20 -0500

    updated file

--- components.js
+++ components.js
@@ -16,16 +16,16 @@ class Comment extends RTCIceCandidate.Component {
     }
 }
 
-class CommentBoc extends React.Component {
+class CommentBox extends React.Component {
     render() {
         return (
           <div className="comment-box">
           <h3>Comments</h3>
-          <h4 className="comment-count">2 commetns</h4>
+          <h4 className="comment-count">2 comments</h4>
           <div className="comment-list">
             <Comment 
                 author="Morgan McCurcuit" body="Great picture!" />
-            <Commet 
+            <Comment 
                 author="Bending Bender" body="Excellent stuff" />
             </div>        
           </div>
